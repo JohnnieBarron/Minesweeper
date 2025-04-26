e56c38e8df5e78e992d840803ac4ff9e47d3bdc2
jbarron5925 <ant.baroon5925@gmail.com>
2025-04-26 11:51:14 -0500

    intial CSS finished for basic fuctionality test, establish JS state variables

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
   /*----- constants -----*/
+  
 
 
   /*----- state variables -----*/
+  let board;
+  let flags;
+  let winner;
 
 
   /*----- cached elements  -----*/
+
+  
 
 
   /*----- event listeners -----*/
