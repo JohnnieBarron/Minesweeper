cbf753c5ec0780c08c0fcb0b7ffe63d6cf2c2d18
jbarron5925 <ant.baroon5925@gmail.com>
2025-04-26 14:20:16 -0500

    add eventlsiteners, board element and debug function

--- js/main.js
+++ js/main.js
@@ -1,24 +1,47 @@
-  /*----- constants -----*/
+/*----- constants -----*/
+  
+/*----- state variables -----*/
+let board;
+let flags;
+let winner;
+
+/*----- cached elements  -----*/
+
+const msgEl = document.querySelector('#message');
+const buttonEl = document.querySelector('.head button');
+const flagEL = document.querySelector('#flags');
+const mineCounterEL = document.querySelector('#mines');
+const boardEls = document.querySelectorAll('section.minefield div');
+
   
 
+/*----- event listeners -----*/
 
-  /*----- state variables -----*/
-  let board;
-  let flags;
-  let winner;
+buttonEl.addEventListener('click', init);
 
-
-  /*----- cached elements  -----*/
-
-  
+ boardEls.forEach((El) => {
+   El.addEventListener('click', handleClick);
+ });
 
 
-  /*----- event listeners -----*/
 
 
-  /*----- functions -----*/
+
+/*----- functions -----*/
 
 
+
+function init(event) {
+  console.log(event.target);
+};
+
+function handleClick(event) {
+  console.log(event.target);
+};
+
+function renderGame() {
+
+};
 
 
 
